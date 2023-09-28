cf3c489b43b93d8729525dbb19f59b56f5dcc92a
Harshit Shukla <shuklaharshit432@gmail.com>
2023-09-28 22:04:16 +0530

    removed mongoDB references. Any dbms can be used

--- app_name/config.py
+++ app_name/config.py
@@ -9,12 +9,6 @@ api_id = int(os.getenv('TELEGRAM_API_ID'))
 api_hash = os.getenv('TELEGRAM_API_HASH')
 bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
 
-mongodb = os.getenv('DATABASE_URL')
-# Initialise the mongodb connection
-client = pymongo.MongoClient(host=mongodb)
-mydb = client["MyDB"]
-myColl = mydb["app_name"]
-
 app = Client(
 	session,
 	api_id = api_id,
